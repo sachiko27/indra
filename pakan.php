f9045916efe7867a7c0ec2cba502df640ce1a0bc
anggaid14 <37369406+anggaid14@users.noreply.github.com>
2018-08-11 06:07:28 +0700

    Add files via upload

---
+++ pakan.php
@@ -0,0 +1,32 @@
+<?php
+// My FB : Manggala Febri Valentino
+function pakan($uid, $usertoken, $jumlah, $wait){
+    $x = 0; 
+    while($x < $jumlah) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"http://h5server.jollychic.com/active/sheepFeed/feed.do?callback=topicCallback1533793176459&appTypeId=0&lang=12&countryCode=ID&appVersion=7.1.1&currency=IDR&terminalType=1&timestamp=1533792012760&cookieId=c81dbd56-5ecf-49ed-9e0e-ef67ad30c1e4&userId=$uid&userToken=$usertoken&token=FfHKVDLRxPrCabMKNVV4KFJ%2BXve5hz0O3cjI7sjZbP3igP5v9%2F0zlAQQ&_=1533793108210");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_REFERER, 'https://h5server.jollychic.com/');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+		echo $server_output."\n";
+        sleep($wait);
+        $x++;
+        flush();
+    }
+}
+print "AWTO MANGANI WEDUS\n\n";
+echo "Jumlah Pakan lu\nInput : ";
+$jumlah = trim(fgets(STDIN));
+echo "User ID?\nInput : ";
+$uid = trim(fgets(STDIN));
+echo "User Token?\nInput : ";
+$usertoken = trim(fgets(STDIN));
+echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
+$jeda = trim(fgets(STDIN));
+$execute = pakan($uid, $usertoken, $jumlah, $jeda);
+print $execute;
+print "WEDUSMU WAREG\n";
+?>
